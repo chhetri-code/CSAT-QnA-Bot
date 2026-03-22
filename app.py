7f7e8ef9e136290af3f7d09934e4564f23252e9b
chhetri-code <chhetri.code@gmail.com>
2026-03-22 15:42:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ st.set_page_config(
     page_icon="💬",
     layout="wide",
     initial_sidebar_state="collapsed",
+    toolbarMode = "auto",
 )
 
 # ── CSS ───────────────────────────────────────────────────────────────────────
