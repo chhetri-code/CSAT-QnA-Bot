046e94ae4f0c1f136f9b0578157c0cc187008cc3
chhetri-code <chhetri.code@gmail.com>
2026-03-22 15:46:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(
     page_title="Voice of our Customers",
     page_icon="💬",
     layout="wide",
-    initial_sidebar_state="collapsed",
+   
 )
 
 # ── CSS ───────────────────────────────────────────────────────────────────────
